df60e7c42c8f1204f9e02952ea6c3bc2eb8601ec
Sungsik Nam <jmyl@me.com>
2025-08-16 17:08:37 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,21 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/674f6d128a9ad3bb1fe4e8acc64775ae557b3fc1";
 
-  outputs = { self, nixpkgs }: {
-    devShell = nixpkgs.lib.mkDevShell {
-      packages = with nixpkgs.pkgs; [
-        hugo
-      ];
+  outputs = { self, nixpkgs }:
+    let
+      systems = nixpkgs.lib.systems.flakeExposed;
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              hugo
+            ];
+          };
+        }
+      );
     };
-  };
 }
 
